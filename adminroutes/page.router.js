52b68a59fb0261f6e3083594ca08bd902247e883
aarifpremad <aarifpremad@gmail.com>
2025-02-06 17:50:58 +0530

    page update

--- adminroutes/page.router.js
+++ adminroutes/page.router.js
@@ -12,11 +12,48 @@ router.get('/teach-pages', async (req, res) => {
     }
 });
 
+// Get Teach Page by ID for editing
+router.get('/teach-pages/:id', async (req, res) => {
+    try {
+        const page = await Page.findById(req.params.id);
+        if (!page) {
+            return res.status(404).json({ error: 'Page not found' });
+        }
+        res.json(page);
+    } catch (error) {
+        console
+        res.status(500).json({ error: 'Error fetching teach page' });
+    }
+});
+
+router.put('/teach-pages/:id', async (req, res) => {
+    try {
+        const { pg_title, pg_url_key, pg_content, meta_title, meta_desc, meta_keywords } = req.body;
+
+        const updatedPage = await Page.findByIdAndUpdate(req.params.id, {
+            pg_title,
+            pg_url_key,
+            pg_content,
+            meta_title,
+            meta_desc,
+            meta_keywords
+        }, { new: true });
+
+        if (!updatedPage) {
+            return res.status(404).json({ error: 'Page not found' });
+        }
+
+        res.json(updatedPage);
+    } catch (error) {
+        res.status(500).json({ error: 'Error updating teach page' });
+    }
+});
+
 // Add a new page
 router.post('/teach-pages', async (req, res) => {
     try {
-        const { pg_title, pg_content, pg_url_key,meta_title,meta_description,meta_keywords } = req.body;
-        const newPage = new Page({ pg_title, pg_content, pg_url_key,meta_title,meta_description,meta_keywords  });
+        const { pg_title, pg_content, pg_url_key,meta_title,meta_desc,meta_keywords } = req.body;
+        const newPage = new Page({ pg_title, pg_content, pg_url_key,meta_title,meta_desc,meta_keywords  });
         await newPage.save();
         res.sendStatus(201);
     } catch (err) {
